63ce256a44d0a6ec33b6dc30b801d5f7695c6aa0
Chris Perrins <christopher.perrins@qa.com>
2019-11-13 13:50:09 +0000

    dto added

--- src/main/java/com/nationwide/shop/dto/ProductDto.java
+++ src/main/java/com/nationwide/shop/dto/ProductDto.java
@@ -1,5 +1,24 @@
 package com.nationwide.shop.dto;
 
 public class ProductDto {
-
+	
+	private String name;
+	private Float price;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Float getPrice() {
+		return price;
+	}
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+	
+	
+	
 }
